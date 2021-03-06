fc1bb8142fdf1e7cb9c28b490395ed495c4148e9
teucer12 <67879443+teucer12@users.noreply.github.com>
2021-03-05 23:32:41 -0600

    Login now functional

--- webappPOC_2.py
+++ webappPOC_2.py
@@ -1,27 +1,55 @@
 import flask
 import subprocess
-import flask_login
+from forms import LoginForm
+from flask_session import Session
+from flask import render_template, session, request
+from netmiko import ConnectHandler
 
 app = flask.Flask(__name__)
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
 @app.route('/')
 def samplefunction():
+    s = flask.session.get('data')
+    print(s)
     return flask.render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        data = {'username': form.username.data, 'password': form.password.data, 'ip_address': form.ip_address.data}
+        flask.session['data'] = data
+        return flask.redirect('/')
+    return flask.render_template('login.html', title='Sign In', form=form)
 
 @app.route('/lab01')
 def lab01():
-    def inner():
-        proc = subprocess.Popen(
-            ['python3 lab_01_remote.py'],
-            shell=True,
-            stdout=subprocess.PIPE,
-            universal_newlines=True
+    s = flask.session.get('data')
+#    def inner():
+    VMR = {'device_type': "linux", 'ip': s['ip_address'], 'username': s['username'], 'password': s['password']}
+
+    # initiate connection
+    net_connect = ConnectHandler(**VMR)
+    output = net_connect.send_command(
+        "curl https://raw.githubusercontent.com/mnjk-inver/Linux-2480-Rebuild/main/lab_01_revised.py | python3",
+    # proc = subprocess.Popen(
+    #     ['python3 lab_01_remote.py'],
+    #     shell=True,
+    #     stdout=subprocess.PIPE,
+    #     universal_newlines=True
         )
-        for line in iter(proc.stdout.readline, ''):
-            yield line + "<br/>\n"
-    return flask.Response(inner(), mimetype='text/html')
+    return f'''
+        <html>
+            <body>
+            <p> <pre>{output}</pre> </p>
+            </body>
+        </html>
+    '''
+ #   return flask.Response(inner(), mimetype='text/html')
 
 @app.route('/lab02')
 def lab02():
